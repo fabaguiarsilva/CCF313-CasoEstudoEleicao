fb00266b0413ade76592350166a058917d324a4a
Fabrício <fabaguiarsilva@gmail.com>
2022-02-22 14:08:50 -0300

    Refactoring na hora de contabilizar os votos

--- src/main/java/br/ufv/caf/modelo/entidade/Urna.java
+++ src/main/java/br/ufv/caf/modelo/entidade/Urna.java
@@ -1,17 +1,21 @@
 package br.ufv.caf.modelo.entidade;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import org.apache.log4j.Logger;
 
 /**
  *
  * @author fabriciosilva
  */
 public class Urna {
+    private static final Logger LOGGER = Logger.getLogger("br.ufv.caf.eleicao");    
     private ArrayList<Cedula> listaCedulas;
+    private ArrayList<VotoConsolidado> votosConsolidados;    
     
     public Urna(){
         listaCedulas = new ArrayList();
+        votosConsolidados =
+                new ArrayList<VotoConsolidado>();        
     }
     
     public boolean isVazia(){
@@ -20,10 +24,17 @@ public class Urna {
     
     public void insereCedula(Cedula cedula){
         listaCedulas.add(cedula);
+        adicionaVoto(cedula);
     }
     
     public void limpaUrna(){
         listaCedulas.clear();
+    }
+    
+    public void adicionaCandidatoZerado(Candidato c){
+        VotoConsolidado voto = 
+                        new VotoConsolidado(c.getNumero());
+        votosConsolidados.add(voto);        
     }
     
     /**
@@ -31,20 +42,24 @@ public class Urna {
      * @return um hashmap em que a chave é o número do
      * candidato, e o valor é a quantidade de votos
      */
-    public HashMap<Integer, Integer> contabilizaVotos(){
-        HashMap<Integer, Integer> votosPorCandidato =
-                new HashMap();
+    public ArrayList<VotoConsolidado> contabilizaVotos(){
         
-        for(Cedula c : listaCedulas){
-            //verifica se o número já foi adicionado
-            if(votosPorCandidato.get(c.getNumeroCandidato())
-                    == null){
-                votosPorCandidato.put(c.getNumeroCandidato(),1);
-            }else{
-                int votoAnterior = votosPorCandidato.get(c.getNumeroCandidato());
-                votosPorCandidato.put(c.getNumeroCandidato(),votoAnterior+1);
-            }
+        for(VotoConsolidado voto : votosConsolidados){
+            LOGGER.info("Candidato "+voto.getNumeroCandidato()+
+                    " teve "+voto.getVotos()+ " votos!");
         }
-        return votosPorCandidato;
+        
+        return votosConsolidados;
+    }
+        
+    private void adicionaVoto(Cedula c){        
+        for(VotoConsolidado voto : votosConsolidados){
+            if(voto.getNumeroCandidato() ==
+                        c.getNumeroCandidato()){
+                voto.incrementaVoto();
+                return;
+            }
+        } 
+        //TODO tratar voto nulo
     }    
 }
